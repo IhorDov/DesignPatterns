e49cef3b6a3168a5ec4e1321ed0a9a626ea93256
IhorDov <93764766+IhorDov@users.noreply.github.com>
2022-04-05 09:53:18 +0200

    Add files via upload
    Added files to repository

---
+++ GameWorld.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace DesignPattern
+{
+    public class GameWorld : Game
+    {
+        private static GameWorld instance;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private List<GameObject> gameObjects = new List<GameObject>();
+
+        private List<GameObject> newGameObjects = new List<GameObject>();
+
+        private List<GameObject> destroyedGameObjects = new List<GameObject>();
+
+        public static float DeltaTime { get; private set; }
+
+        public GraphicsDeviceManager Graphics
+        {
+            get { return graphics; }
+            set { graphics = value; }
+        }
+
+        public static GameWorld Instance 
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameWorld();
+                }
+                return instance;
+            }            
+        }
+
+        private GameWorld()
+        {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            ScreenSize();
+
+            Director director = new Director(new PlayerBuilder());
+            gameObjects.Add(director.Construct());
+
+
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                gameObjects[i].Awake();
+            }
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                gameObjects[i].Start();
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            DeltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                gameObjects[i].Update(gameTime);
+            }
+
+            base.Update(gameTime);
+
+            Cleanup();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                gameObjects[i].Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void Instantiate(GameObject go)
+        {
+            newGameObjects.Add(go);
+        }
+
+        public void Destroy(GameObject go)
+        {
+            destroyedGameObjects.Add(go);
+        }
+
+        private void Cleanup()
+        {
+            for (int i = 0; i < newGameObjects.Count; i++)
+            {
+                gameObjects.Add(newGameObjects[i]);
+                newGameObjects[i].Awake();
+                newGameObjects[i].Start();
+
+            }
+
+            for (int i = 0; i < destroyedGameObjects.Count; i++)
+            {
+                gameObjects.Remove(destroyedGameObjects[i]);
+            }
+            destroyedGameObjects.Clear();
+            newGameObjects.Clear();
+        }
+
+        private void ScreenSize()
+        {
+            Graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height - 150; //sets the height of the window
+            Graphics.PreferredBackBufferWidth = Graphics.PreferredBackBufferHeight / 300 * 600; //sets the width of the window
+            Graphics.ApplyChanges(); //applies the changes
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            GameWorld.Instance.Run();
+        }
+    }
+}
